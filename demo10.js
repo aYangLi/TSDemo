2ad85a8c803ada8e58c1e76f447ddbc5c96be6d5
liChenYang <chenyang.li@dmall.com>
2019-10-17 17:38:44 +0800

    正则

---
+++ demo10.js
@@ -0,0 +1,11 @@
+"use strict";
+// RegExp string
+// 构造函数声明法
+var reg1 = new RegExp('aYang');
+console.log(reg1);
+var reg2 = new RegExp('aYang', 'gi');
+console.log(reg2);
+// 字面量声明
+var reg3 = /aYang/i;
+console.log(reg3);
+// test(string)  exec(string)
---
+++ demo10.ts
@@ -0,0 +1,15 @@
+// RegExp string
+
+// 构造函数声明法
+let reg1:RegExp = new RegExp('aYang');
+console.log(reg1);
+let reg2:RegExp = new RegExp('aYang','gi');
+console.log(reg2);
+
+
+// 字面量声明
+let reg3:RegExp = /aYang/i;
+console.log(reg3);
+
+// test(string)  exec(string)
+
