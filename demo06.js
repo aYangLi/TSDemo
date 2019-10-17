a40740f3854dd8e902735db31a0b0b9844a8079b
liChenYang <chenyang.li@dmall.com>
2019-10-17 15:54:46 +0800

    作用域

---
+++ demo06.js
@@ -0,0 +1,7 @@
+"use strict";
+// 变量的作用域，函数划分
+function zenXingu() {
+    var yangZi = '刘德华';
+    console.log(yangZi);
+}
+zenXingu();
---
+++ demo06.ts
@@ -0,0 +1,8 @@
+// 变量的作用域，函数划分
+
+function zenXingu():void {
+    const yangZi: string = '刘德华';
+    console.log(yangZi);
+}
+
+zenXingu();
