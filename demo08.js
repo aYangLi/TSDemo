52d33e61ab5ca0e3e15861046553ede810282645
liChenYang <chenyang.li@dmall.com>
2019-10-17 17:17:30 +0800

    字符串

---
+++ demo08.js
@@ -0,0 +1,10 @@
+"use strict";
+var aYang02 = 'aYang';
+var aYang01 = new String('aYang01');
+console.log(aYang01.length);
+console.log(aYang02.length);
+// 查找字符串 indexOf
+var something = '清早起床，小鸟在飞翔。小鸟';
+var xiaoNiao = '小鸟';
+console.log(something.indexOf(xiaoNiao));
+console.log(something.lastIndexOf(xiaoNiao));
---
+++ demo08.ts
@@ -0,0 +1,12 @@
+let aYang02:string = 'aYang';
+let aYang01:String = new String('aYang01');
+
+console.log(aYang01.length);
+console.log(aYang02.length);
+
+// 查找字符串 indexOf
+let something:string = '清早起床，小鸟在飞翔。小鸟';
+let xiaoNiao:string = '小鸟';
+
+console.log(something.indexOf(xiaoNiao));
+console.log(something.lastIndexOf(xiaoNiao));
